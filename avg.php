ecf5d0001a7c8a0b2c0dfd9989b3dd0a08b53231
vdi-terminal <vdi-terminal@EDU-WSB.local>
2021-10-17 11:24:18 +0200

    master

--- avg.php
+++ avg.php
@@ -1,11 +1,3 @@
-<?php
-
-	function sredni(int a, int b){
-		return (a+b)/2;
-	}
-	
-	function wazona(int a, int b){
-		return (6*a+a*b)/10;
-	}
-	
-?>
+function sredni(int a, int b){
+	return (a+b)/2;
+}
